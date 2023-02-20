06ae9ac242a8e808af86437b6498cc827697a6ef
Vitor Otero <vitor@v0t.org>
2023-02-20 12:43:48 +0000

    🎉  Secao02 - Inicio do projeto

---
+++ secao02/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"msg" : "FastAPI starts"}
+
+
+if __name__ == '__main__':
+    import uvicorn
+    
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
